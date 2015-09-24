c3ab28cd87c9a088c2b6233606d99b76106cef62
Ohohcakester <ohohgit@gmail.com>
2015-09-24 23:14:23 +0800

    Add shooting

---
+++ Part2/Assets/Shooting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab_bullet;
+
+    private float bulletSpeed = 20f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Z))
+	    {
+	        var bullet = Instantiate(prefab_bullet, transform.position, prefab_bullet.transform.rotation) as GameObject;
+	        var bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+	        bulletRigidbody.velocity = Vector3.up*bulletSpeed;
+	    }
+	}
+}
