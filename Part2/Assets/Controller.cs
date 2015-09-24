20bd9b0ebcb2da363b227c3e1bb578f08ff4077c
Ohohcakester <ohohgit@gmail.com>
2015-09-24 18:26:01 +0800

    controller.cs script

--- Part2/Assets/Controller.cs
+++ Part2/Assets/Controller.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
 
 	// Use this for initialization
 	void Start ()
@@ -13,5 +15,26 @@ public class Controller : MonoBehaviour {
 	void Update ()
 	{
 	    //Debug.Log("Hello World - Update!");
+	    var rigidbody = GetComponent<Rigidbody2D>();
+        var velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity += Vector3.up * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity += Vector3.down * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity += Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity += Vector3.right * speed;
+        }
+
+	    rigidbody.velocity = velocity;
 	}
 }
