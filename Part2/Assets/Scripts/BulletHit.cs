d56ef1b4ad4eca9dcafcff816d31f4d0ce65c296
Ohohcakester <ohohgit@gmail.com>
2015-09-25 11:34:46 +0800

    Add Hit Points

--- Part2/Assets/Scripts/BulletHit.cs
+++ Part2/Assets/Scripts/BulletHit.cs
@@ -6,16 +6,6 @@ public class BulletHit : MonoBehaviour
 
     public bool isEnemyBullet;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
-
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (isEnemyBullet)
@@ -27,7 +17,11 @@ public class BulletHit : MonoBehaviour
             if (collider.tag != "Enemy") return;
         }
 
-        Destroy(gameObject);
-        Destroy(collider.gameObject);
+        var hitPoints = collider.GetComponent<HitPoints>();
+        if (hitPoints != null)
+        {
+            Destroy(gameObject);
+            hitPoints.TakeDamage(1);
+        }
     }
 }
---
+++ Part2/Assets/Scripts/HitPoints.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPoints : MonoBehaviour {
+
+    [SerializeField]
+    private int maxHp = 1;
+
+    private int hp;
+
+	// Use this for initialization
+	void Start () {
+        hp = maxHp;
+	}
+
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
