ce9e38fe447fe857e1ab9ccf1359b73549a6b4bf
Ohohcakester <ohohgit@gmail.com>
2015-09-25 10:24:03 +0800

    Add bullet expiry

---
+++ Part2/Assets/Scripts/Expiry.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Expiry : MonoBehaviour
+{
+    [SerializeField]
+    private float expireDuration = 10f;
+
+    private float expireTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    expireTime = Time.time + expireDuration;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Time.time > expireTime)
+	    {
+	        Destroy(this.gameObject);
+	    }
+	}
+}
